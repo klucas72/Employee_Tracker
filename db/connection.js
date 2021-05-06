606dd79424a98a581f78fcca52d97c64b515fd54
klucas72 <klucas72@hotmail.com>
2021-05-06 16:23:44 -0400

    moved .env file

--- db/connection.js
+++ db/connection.js
@@ -6,6 +6,7 @@ const mysql2 = require("mysql2");
 
 const connection = mysql2.createConnection({
     host: "localhost",
+    port: 3306,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
