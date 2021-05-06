7763ab57d0ffe8b585e273f0881556940269587d
klucas72 <klucas72@hotmail.com>
2021-05-05 21:31:23 -0400

    building index.js and connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,19 @@ require('dotenv').config();
 
 const util = require("util");
 const mysql = require("mysql");
+const mysql2 = require("mysql2");
 
-const conection = mysql.createConnection({
+const connection = mysql2.createConnection({
     host: "localhost",
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
 });
 
-connection.connect();
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadID}\n`)
+});
 
 connection.query = util.promisify(connection.query);
 
--- db/index.js
+++ db/index.js
@@ -1,19 +1,123 @@
 const connection = require("./connection");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const Choice = require("inquirer/lib/objects/choice");
 
-class DB {
-    constructor(connection) {
-        this.connection = connection;
-    }
+function choices() {
+    inquirer.prompt({
+        message: "please Select from the Following:",
+        type: "list",
+        choices: ["View Employees", "View Departments", "View Roles", "Add Employee(s)", "Add Role(s)", "Add Department(s)", "Update Role(s)", "Exit"],
+        name: "Choice"
 
-    findAllEmployees() {
-        return this.connection.query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manger.id = employee.manager_id;"
-        )
-    }
+    }).then(responses => {
+        console.log(responses.choices);
+        switch (responses.choices) {
+            case "View Employees":
+                viewEmployees();
+                break;
 
-    findAllPossibleManagers(employeeID) {
-        return this.connection.query(
-            "SELECT id, first_name, last_name FROM employee WHERE id ≠ ?', employeeId"
-        );
-    }
+            case "View Departments":
+                viewDepartments();
+                break;
+
+            case "View Roles":
+                viewRoles();
+                break;
+
+            case "Add Employee(s)":
+                addEmployee();
+                break;
+
+            case "Add Role(s)":
+                addRoles();
+                break;
+
+            case "Add Department(s)":
+                addDepartment();
+                break;
+
+            case "Update Role(s)":
+                updateRole();
+                break;
+
+            default:
+                connection.end();
+                break;
+        }
+    })
 }
+
+function viewEmployees() {
+    connection.query('SELECT * FROM employee', (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        choices()
+    })
+}
+
+function viewDepartments() {
+    connection.query('SELECT * FROM department', (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        choices();
+    })
+}
+
+function viewRoles() {
+    connection.query('SELECT * FROM role', (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        choices()
+    })
+}
+
+function addEmployee() {
+    inquirer.prompt([{
+        type: "input",
+        name: "firstName",
+        message: "Employee's first name?"
+    },
+    {
+        type: "input",
+        name: "lastName",
+        message: "Employee's last name?"
+    },
+    {
+        type: "number",
+        name: "roleID",
+        message: "Employee's Role ID?"
+    },
+    {
+        type: "number",
+        name: "managerID",
+        message: "Employee's Manager ID?"
+    }
+    ]).then(function (result) {
+        connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id VALUES (?, ?, ?, ?)', [result.firsName, result.lastName, result.roleID, result.managerID], function (err, data) {
+            if (err) throw err;
+            console.table(data, "New employee inserted!");
+            choices();
+        })
+    })
+}
+
+function addDepartment() {
+    inquirer.prompt([{
+        type: "input",
+        name: "department",
+        Message: "Please enter the department name."
+    }]).then(function (result) {
+        connection.query('INSERT INTO department (name) VALUES (?)', [result.department], function (err, data) {
+            if (err) throw err;
+            console.table(data, "New department inserted!");
+        })
+    })
+}
+
+// function addRoles() {
+//     inquirer.prompt([{
+
+//     }])
+// }
