3ec82a2fca44f95c54b1c5cf48d44e95a86fded2
klucas72 <klucas72@hotmail.com>
2021-05-08 11:38:31 -0400

    added more psuedo code for files

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
-
+//required dependencies
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
@@ -6,6 +6,7 @@ const util = require("util");
 const mysql = require("mysql");
 const mysql2 = require("mysql2");
 
+//creating connection to the database
 const connection = mysql2.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,12 +14,13 @@ const connection = mysql2.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });
-
+//connection status to database
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
 });
 
+//allowing promises for connection queries
 connection.query = util.promisify(connection.query);
 
 module.exports = connection;
